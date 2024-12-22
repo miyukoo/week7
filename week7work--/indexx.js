27ed3cb3ca1cbd37e5bb345a116f17e1dca12d5c
chelseap <138651719+miyukoo@users.noreply.github.com>
2024-12-21 23:05:33 -0500

    Edit the js html from week 5(implement weather des)

--- week7work--/indexx.js
+++ week7work--/indexx.js
@@ -1,20 +1,41 @@
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = Math.round(response.data.temperature.current);
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
-}
-
 function search(event) {
   event.preventDefault();
+
   let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
+  let cityElement = document.querySelector("#current-city");
+  
+  let cityName = searchInputElement.value;
+  cityElement.innerHTML = cityName;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let Key = "5bo34abbe03172f23694c7869ee00et8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${Key}`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(weather);
+}
+
+function weather(response) {
+  let temperature = response.data.temperature.current;
+  let weatherDescription = response.data.condition.description;
+  let humidity = response.data.temperature.humidity;
+  let windspeed = response.data.wind.speed;
+
+  let weatherDetailsElement = document.querySelector(".current-details");
+
+
+  let temperatureElement = document.querySelector(".current-temperature-value");
+  let temperatureUnitElement = document.querySelector(
+    ".current-temperature-unit"
+  );
+  let iconElement = document.querySelector("#weather-icon");
+  let icon = response.data.condition.icon;
+  let iconurl = `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${icon}.png`;
+  iconElement.setAttribute("src", iconurl);
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  temperatureUnitElement.innerHTML = "°C";
+ 
+  weatherDetailsElement.innerHTML=`${formatDate(new Date(response.data.time*1000))}, ${weatherDescription} <br />
+  Humidity: <strong>${humidity}%</strong>, Wind: <strong>${windspeed} km/h<strong/>`;
 }
 
 function formatDate(date) {
@@ -49,5 +70,4 @@ searchForm.addEventListener("submit", search);
 
 let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
-
 currentDateELement.innerHTML = formatDate(currentDate);
